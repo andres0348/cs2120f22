2a5cf1e6a7bc558f997d824ed4cb90b177d31670
Andres <acz9et@virginia.edu>
2022-09-08 18:37:28 +0000

    9/6

--- src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_sqrt_z3.py
+++ src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_sqrt_z3.py
@@ -1,12 +1,25 @@
+from unicodedata import name
 from z3 import *
 
 def sqrt(n) :
     sqrtn = Real('sqrtn')
     s = Solver()
-    s.add(True) # replace True with required declarative spec
+    s.add(And((sqrtn ** 2) == n), sqrtn >= 0) # replace True with required declarative spec
     isSat = s.check()
-    if (isSat) :
+    if (isSat == sat) :
         return s.model()
     return -1
     
-print(sqrt(9))
+print(sqrt(64))
+
+
+def neg_sqrt(n) :
+    sqrtn = Real('sqrtn')
+    s = Solver()
+    s.add(And((sqrtn ** 2) == n), sqrtn <= 0) # replace True with required declarative spec
+    isSat = s.check()
+    if (isSat == sat) :
+        return s.model()
+    return -1
+
+print(neg_sqrt(17))
